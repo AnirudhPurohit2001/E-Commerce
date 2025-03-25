b924cd4cf6350b5977391e7f35a6097fe7aa7bee
Anirudh Purohit <ani.purohit2001@gmail.com>
2025-03-25 13:38:51 +0530

    First Commit

---
+++ frontend/src/pages/Verify.jsx
@@ -0,0 +1,48 @@
+import React, { useContext, useEffect } from 'react'
+import { ShopContext } from '../context/ShopContext'
+import { useSearchParams } from 'react-router-dom'
+import { backendURL } from '../../../admin/src/App'
+import { toast } from 'react-toastify'
+import axios from 'axios'
+import Login from '../../../admin/src/components/Login'
+
+const Verify = () => {
+
+    const { navigate, token, setCartItems, bakendURL } = useContext(ShopContext)
+    const [searchParams, setSearchParams] = useSearchParams()
+
+    const success = searchParams.get('success')
+    const orderId = searchParams.get('orderId')
+
+    const verifyPayment = async () => {
+        try {
+            if (!token) {
+                return null
+            }
+
+            const response = await axios.post(backendURL + '/api/order/verifyStripe', { success, orderId }, { headers: { token } })
+            console.log(response.data);
+
+            if (response.data.success) {
+                setCartItems({})
+                navigate('/orders')
+            } else {
+                navigate('/cart')
+            }
+        } catch (error) {
+            console.log(error)
+            toast.error(error.message)
+        }
+    }
+
+    useEffect(() => {
+        verifyPayment()
+    }, [token])
+    return (
+        <div>
+            Verifying Payment ....
+        </div>
+    )
+}
+
+export default Verify
