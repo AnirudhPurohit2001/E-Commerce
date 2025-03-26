760cee27916ee38f415a60bf653c0a3bac544d57
Anirudh Purohit <ani.purohit2001@gmail.com>
2025-03-26 12:02:26 +0530

    configured vite.config file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   optimizeDeps: {
-    include: ['axios', 'react-router-dom'],
+    include: ['axios', 'react-router-dom', 'react-toastify'],
   }, build: {
     rollupOptions: {
       external: ['axios', 'react-toastify'],
